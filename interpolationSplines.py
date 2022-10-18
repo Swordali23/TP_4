18e38d416017f27e75c81b98b96efd1c9ae8e401
Swordali23 <alireza.atefi@edu.ece.fr>
2022-10-18 18:07:47 +0200

    Update interpolationSplines.py

--- interpolationSplines.py
+++ interpolationSplines.py
@@ -1,6 +1,6 @@
 from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
-from numpy import nbytes, zeros,array,dot,linspace, linalg, hstack
+from numpy import concatenate, nbytes, zeros,array,dot,linspace, linalg, hstack
 import matplotlib.pyplot as plt
 
 
@@ -17,12 +17,10 @@ def interpolationSplines(x, y, nb):
     m = zeros(n-1)
     a = zeros(n-1)
     b = zeros(n-1)
-    Yp = zeros(nb*n)
-    xp = [0]
-    yp = [0]
+    Yp = zeros(nb)
 
     h[0] = x[1] - x[0]
-    f[0] = (y[1] - y[0]) / h[0]
+    f[0] = 6*(y[1] - y[0]) / h[0]
     T[0][0] = 2 * h[0]
     
     ## Construction de la matrice T ##
@@ -30,39 +28,55 @@ def interpolationSplines(x, y, nb):
         h[i] = x[i+1] - x[i]
         T[i][i] = 2 * (h[i] + h[i-1])
         T[i][i-1] = h[i-1]
-        T[i-1][i] = h[i-1]
-        f[i] = 6*((y[i+1] - y[i])/h[i] - (y[i] - y[i-1])/h[i-1])
+        T[i-1][i] = h[i]
+        f[i] = 6 * (y[i+1] - y[i]) / h[i] - 6 * (y[i] - y[i-1]) / h[i-1]
 
-    print(T)
-    print(f)
+    print('T = \n', T)
+    print('f = ', f)
     ## Construction du vecteur m ##
     m = dot(linalg.inv(T), f)
-    print(m)
+    print('m = ', m)
 
     ## Construction des coefficients a et b ##
     for j in range(0, n-2):
-        a[j] = (y[j+1] - y[j])/h[j] - h[j]*(2*m[j] + m[j+1])/6
-        b[j] = y[j] - m[j]*h[j]**2/6
+        a[j] = (y[j+1] - y[j])/h[j] - h[j]*(m[j+1] - m[i])/6
+        b[j] = y[j] - (m[j]*h[j]**2)/6
+    a[n-2] = (y[n-1] - y[n-2])/h[n-2] + h[n-2]*(m[n-2])/6
+    b[n-2] = y[n-2] - m[n-2]*h[n-2]**2/6
     
-    print(a)
-    print(b)
+    print('a = ', a)
+    print('b = ', b)
 
     ## Construction de la spline ##
-    for k in range(n-1):
+    for k in range(0, n-2):
         Xp = linspace(x[k], x[k+1], nb)
-        for l in range(nb):
-            Yp[k] = m[k+1]*((Xp[l] - x[k])**3)/6*h[k] + m[k]*((Xp[l] - x[k])**3)/6*h[k] + a[k]*(Xp[l] - x[k]) + b[k]
+        Yp = m[k+1]*((Xp - x[k])**3)/6*h[k] + m[k]*((x[k+1] - Xp)**3)/6*h[k] + a[k]*(Xp - x[k]) + b[k]
 
-        xp = hstack((xp, Xp))
-        yp = hstack((yp, Yp))
+
+        # for l in range(nb):
+        #     if(k != n-2):
+        #         Yp[l] = m[k+1]*((Xp[l] - x[k])**3)/6*h[k] + m[k]*((x[k+1] - Xp[l])**3)/6*h[k] + a[k]*(Xp[l] - x[k]) + b[k]
+        #     else:
+        #         Yp[l] = m[k]*((x[k+1] - Xp[l])**3)/6*h[k] + a[k]*(Xp[l] - x[k]) + b[k]
+        
+        if (k == 0):
+            yp = Yp
+            xp = linspace(x[0], x[1], nb)
+        else:
+            xp = concatenate((xp, Xp))
+            yp = concatenate((yp, Yp))
+    print('xp = ', xp)
+    print('yp = ', yp)
 
     return xp, yp
 
 
 x = array([1, 2, 5, 6, 7, 8, 10, 13, 17])
 y = array([3, 3.7, 3.9, 4.2, 5.7, 6.6, 7.1, 6.7, 4.5])
-nb = 100
-[xp, yp] = interpolationSplines(x, y, 100)
+nb = 3
+
+[xp, yp] = interpolationSplines(x, y, nb)
 plt.plot(xp, yp)
+plt.show()
 # for i in range(len(xp)):
 #     plt.plot(xp[i], yp[i])
