0d540351337d13e955b11dc64dec9235cba874eb
Swordali23 <alireza.atefi@edu.ece.fr>
2022-11-05 16:07:43 +0100

    finish

---
+++ execution.py
@@ -0,0 +1,29 @@
+from interpolationSplines import interpolationSplines
+from interpolationSplines2 import interpolationSplines2
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import nbytes, zeros,array,dot,linspace, linalg
+
+
+nb = 300
+
+## Courbe 1 ##
+x1 = array([1, 2, 5, 6, 7, 8, 10, 13, 17])
+y1 = array([3, 3.7, 3.9, 4.2, 5.7, 6.6, 7.1, 6.7, 4.5])
+[X1, Y1] = interpolationSplines2(x1, y1, nb)
+
+## Courbe 2 ##
+x2 = array([17, 20, 23, 24, 25, 27, 27.7])
+y2 = array([4.5, 7, 6.1, 5.6, 5.8, 5.2, 4.1])
+[X2, Y2] = interpolationSplines2(x2, y2, nb)
+
+## Courbe 3 ##
+# x3 = array([27.7, 28, 29, 30])
+# y3 = array([4.1, 4.3, 4.1, 3])
+# [X3, Y3] = interpolationSplines2(x3, y3, nb)
+
+
+## tracer ##
+plt.plot(x1, y1, X1, Y1, x2, y2, X2, Y2)
+plt.title("Mon Chien Numérique")
+plt.show()
--- interpolationSplines2.py
+++ interpolationSplines2.py
@@ -4,45 +4,45 @@ import numpy as np
 from numpy import concatenate, nbytes, zeros,array,dot,linspace, linalg, hstack
 import matplotlib.pyplot as plt
 
-def interpolationSplines(x, y, nb):
+def interpolationSplines2(x, y, nb):
 
     # x: array of x values
     # y: array of y values
     # n: number of points in the spline
 
     n = len(x)-1
-    T = zeros((n-1, n-1))
-    h = zeros(n)
-    f = zeros(n-1)
-    m = zeros(n-1)
-    a = zeros(n)
-    b = zeros(n)
+    T = zeros((n-2, n-2))
+    h = zeros(n-1)
+    f = zeros(n-2)
+    m = zeros(n-2)
+    a = zeros(n-1)
+    b = zeros(n-1)
     xp = array([])
     yp = array([])
 
     ## Construction du vecteur h ##
-    for l in range(0, n):
+    for l in range(0, n-1):
         h[l] = x[l+1] - x[l]
     
     print(h)
 
     ## Construction de la matrice T ##
-    for i in range(1, n-1):
+    for i in range(1, n-2):
         T[i][i] = 2 * (h[i] + h[i-1])
 
     T[0][0] = 2 * (h[0] + h[1])
     
-    for p in range(1, n-2):
+    for p in range(1, n-3):
         T[p][p+1] = h[p]
     T[0][1] = h[1]
 
-    for q in range(1, n-1):
+    for q in range(1, n-2):
         T[q][q-1] = h[q-1]
 
     ## Construction du vecteur f
-    for j in range(1, n-1):
+    for j in range(1, n-2):
         f[j-1] = 6 * (y[j+1] - y[j]) / h[j] - 6 * (y[j] - y[j-1]) / h[j-1]
-    f[n-2] = 6 * (y[n-3] - y[n-2]) / h[n-3] - 6 * (y[n-2] - y[n-3]) / h[n-3]
+    f[n-3] = 6 * (y[n-4] - y[n-3]) / h[n-4] - 6 * (y[n-3] - y[n-4]) / h[n-4]
 
     print('T = \n', T)
     print('f = ', f)
@@ -56,7 +56,7 @@ def interpolationSplines(x, y, nb):
     print(len(m))
 
     ## Construction des coefficients a et b ##
-    for t in range(0, n):
+    for t in range(0, n-1):
         a[t] = (y[t+1] - y[t])/h[t] - h[t]*(m[t+1] - m[t])/6
         b[t] = y[t] - (m[t]*h[t]**2)/6
     
@@ -64,7 +64,7 @@ def interpolationSplines(x, y, nb):
     print('b = ', b)
 
     ## Construction de la spline ##
-    for k in range(0, n):
+    for k in range(0, n-1):
         
         Xp = linspace(x[k], x[k+1], nb)
         # xp = np.append(xp, Xp)
@@ -72,9 +72,9 @@ def interpolationSplines(x, y, nb):
         print('k = ', k)
         print('Xp = ', Xp)
         print('Yp = ', Yp)
-        if(k != 0):
-            Xp = np.delete(Xp, 0)
-            Yp = np.delete(Yp, 0)
+        # if(k != 0):
+        #     Xp = np.delete(Xp, 0)
+        #     Yp = np.delete(Yp, 0)
         xp = np.append(xp, Xp)
         yp = np.append(yp, Yp)
 
